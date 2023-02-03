87b68bedb02983f6a7d43234183570b41b14e68c
Jonathan Marrero <Jonathanmarrero24@gmail.com>
2023-02-03 18:07:16 -0500

    user can use teeth to cut grass

---
+++ landscaper.py
@@ -0,0 +1,20 @@
+print("Welcome to the Landscaping Business Game!")
+
+player_name = input("What's your name? ")
+
+print(f"Hi {player_name}, let's start the business.")
+
+money = 0
+tools = "teeth"
+team = False
+
+while True:
+
+    print(f"You currently have ${money} and are using {tools}.")
+    action = input("What would you like to do today? (work/upgrade/hire) ")
+
+    if action == "work":
+        if tools == "teeth":
+            print("You spend the day cutting lawns with your teeth and earn $1.")
+
+            money += 1
